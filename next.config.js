b1d97b764f2f4218411acb88e5ec9f0a43bdb898
Raushan Kumar <147327480+raushan0785@users.noreply.github.com>
2025-10-11 21:09:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,12 @@ const withPWA = require("next-pwa")({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Prevent ESLint from breaking the production build
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
+    // Fix for modules that don't work in Webpack (like canvas, encoding)
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
     return config;
