e5f9047a19521775d4ba46e9550aeadb07ebd33e
Raushan Kumar <147327480+raushan0785@users.noreply.github.com>
2025-10-12 14:57:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// next.config.js
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
@@ -6,27 +5,22 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
-    // Prevent ESLint from breaking the production build
     ignoreDuringBuilds: true,
   },
   webpack: (config) => {
-    // Fix for modules that don't work in Webpack (like canvas, encoding)
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
     return config;
   },
   experimental: {
-    serverComponentsExternalPackages: ['@react-pdf/renderer'],
+    serverComponentsExternalPackages: ["@react-pdf/renderer"],
   },
-};
-
-// Configure PWA
-export default withPWA({
-  ...nextConfig,
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
   },
-});
+};
+
+export default withPWA(nextConfig);
